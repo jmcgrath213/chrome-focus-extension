7a9db55a20b875e921ef356902f36bbfa87cb7ad
jmcgrath213 <jackmcg213@gmail.com>
2022-09-16 16:55:17 -0700

    added in new logo and made UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,21 @@ import {
   CheckboxGroup,
   Checkbox,
   Icon,
-  Switch
+  Switch,
+  Button
 } from '@chakra-ui/react';
-import { GiSpellBook, GiBookmark } from 'react-icons/gi';
+import { GiSpellBook, GiBookshelf } from 'react-icons/gi';
 import './App.css';
 
 function App() {
   const [appIsOn, setAppIsOn] = useState(true);
+  const [focusTime, setFocusTime] = useState("");
+  const [breakTime, setBreakTime] = useState("");
 
   return (
     <Box 
       backdropBlur="md"
-      backgroundColor="ivory"
+      backgroundColor="whitesmoke"
       alignContent="center"
     >
       <VStack>
@@ -31,32 +34,41 @@ function App() {
           <VStack>
             <Icon 
               onClick={() => setAppIsOn(!appIsOn)}
-              as={appIsOn ? GiSpellBook : GiBookmark}
+              as={appIsOn ? GiSpellBook : GiBookshelf}
               color={appIsOn ? "green.500" : "gray.700"}
               boxSize={10}
+              aria-label='On/Off Toggle'
             />
-            <Switch onChange={() => setAppIsOn(!appIsOn)} isChecked={appIsOn} size="md" colorScheme="green" />
+            <Switch
+              onChange={() => setAppIsOn(!appIsOn)}
+              isChecked={appIsOn}
+              size="md"
+              colorScheme="green"
+              aria-label='On/Off Toggle'
+            />
           </VStack>
         </HStack>
-        <CircularProgress value={30} color='orange.400' size="200px" thickness='10px'>
-          <CircularProgressLabel>30%</CircularProgressLabel>
+        <CircularProgress value={30} color="red.500" size="200px" thickness='12px'>
+          <CircularProgressLabel fontSize="lg">0:59:34</CircularProgressLabel>
         </CircularProgress>
-        <Text as="u" fontSize="2xl">Ratio</Text>
+        <Text as="u" fontSize="xl">Ratio</Text>
         <HStack>
           <VStack>
             <Text fontSize="md">Focus Time</Text>
             <Select />
           </VStack>
-          <Text pt="20px">to</Text>
+          <Text pt="30px" fontSize="md">to</Text>
           <VStack>
             <Text fontSize="md">Break Time</Text>
             <Select />
           </VStack>
         </HStack>
-        <Text as="u" fontSize="2xl">Blocked Sites</Text>
+        <Button size="xs" colorScheme="green">Enter</Button>
+        <Text as="u" fontSize="xl">Blocked Sites</Text>
         <CheckboxGroup>
-          <VStack>
+          <VStack spacing={1} alignItems="flex-start">
             <Checkbox>Test</Checkbox>
+            <Checkbox>Test 2</Checkbox>
           </VStack>
         </CheckboxGroup>
       </VStack>
