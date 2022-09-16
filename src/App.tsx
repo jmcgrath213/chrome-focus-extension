48fd4d5951b13f6b66bfc31531577172017b15d0
jmcgrath213 <jackmcg213@gmail.com>
2022-09-16 09:56:13 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import { Box, Heading, CircularProgress, CircularProgressLabel } from '@chakra-ui/react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Heading>Test Header</Heading>
+      <CircularProgress value={30} color='orange.400' thickness='12px'>
+        <CircularProgressLabel>30%</CircularProgressLabel>
+      </CircularProgress>
+    </Box>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
