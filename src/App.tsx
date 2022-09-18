21eb3dd888d523fdaaf6403179f980661341b723
jmcgrath213 <jackmcg213@gmail.com>
2022-09-18 13:00:35 -0700

    added in focus select and connected in the countdown

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,51 @@ function App() {
   const [appIsOn, setAppIsOn] = useState(true);
   const [countdownStarted, setCountdownStarted] = useState(false);
   const [focusTime, setFocusTime] = useState(0);
-  const [breakTime, setBreakTime] = useState(0);
   const countdownRef = useRef<any>();
+
+  const focusOptions = [
+    {
+      label: "30min",
+      timeInMil: 1800000
+    },
+    {
+      label: "45min",
+      timeInMil: 2700000
+    },
+    {
+      label: "1hr",
+      timeInMil: 3600000
+    },
+    {
+      label: "1hr 15min",
+      timeInMil: 4500000
+    },
+    {
+      label: "1hr 30min",
+      timeInMil: 5400000
+    },
+    {
+      label: "1hr 45min",
+      timeInMil: 6300000
+    },
+    {
+      label: "2hr",
+      timeInMil: 7200000
+    },
+    {
+      label: "2hr 30min",
+      timeInMil: 9000000
+    },
+    {
+      label: "3hrs",
+      timeInMil: 10800000
+    },
+  ]
+
+  const getFocusValue = () => {
+    const value = (document.getElementById('focusSelect') as HTMLInputElement).value;
+    setFocusTime(parseInt(value));
+  }
 
   return (
     <Box 
@@ -54,7 +97,7 @@ function App() {
         <CircularProgress value={30} color="red.500" size="200px" thickness='12px'>
           <CircularProgressLabel fontSize="lg">
             <Countdown
-              date={Date.now() + 100000}
+              date={Date.now() + focusTime}
               autoStart={false}
               ref={countdownRef}
               renderer={props => 
@@ -71,6 +114,7 @@ function App() {
               <Button
                 size="xs"
                 colorScheme="red"
+                isDisabled={focusTime === 0}
                 onClick={() => {
                   setCountdownStarted(true);
                   countdownRef.current.start();
@@ -85,12 +129,18 @@ function App() {
         <HStack>
           <VStack>
             <Text fontSize="md">Focus Time</Text>
-            <Select onChange={() => setFocusTime(1)} />
+            <Select id="focusSelect" placeholder='Set Focus Time' onChange={() => getFocusValue()}>
+              {focusOptions.map((option, i) => {
+                return (
+                  <option key={i} value={option.timeInMil}>{option.label}</option>
+                )
+              })}
+            </Select>
           </VStack>
           <Text pt="30px" fontSize="md">to</Text>
           <VStack>
             <Text fontSize="md">Break Time</Text>
-            <Select onChange={() => setBreakTime(1)}/>
+            <Select />
           </VStack>
         </HStack>
         <Button size="xs" colorScheme="green">Enter</Button>
